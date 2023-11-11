eca11f5d802e48fcc89fd3f9a4395d4135757437
efilipe <efilipe@magnasistemas.com.br>
2023-11-10 21:24:16 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTZ_Transports.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
